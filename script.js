88208d193a3770500ffc29d4dc6ec4f7b1021f45
Matthew Yu <matthewyu@Matthews-MacBook-Pro-6.local>
2024-03-18 16:20:10 -0400

    Added win condition

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let gridSpaces = document.querySelectorAll(".board-space")
-const board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+let board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 let playerTurn = 0;
 
 //click on grid - place piece
 gridSpaces.forEach(grid => {
-    grid.addEventListener("click", (e, index) => {
+    grid.addEventListener("click", () => {
         if(grid.childNodes.length === 0) {
         
             let playerPiece = ""
@@ -20,21 +20,49 @@ gridSpaces.forEach(grid => {
                 playerTurn--
             }
 
+            index = Array.from(gridSpaces).indexOf(grid)
+            board[Math.floor(index/3)][index%3] = playerPiece
             tempPiece.textContent = playerPiece
             grid.appendChild(tempPiece)
-            board[index] = playerPiece
-            checkWinner()
+            
+            //There is a winner
+            if(checkWinner() !== -1) {
+                resultScreen()
+            }
         }
     })
 })
 
 //check winner
 const checkWinner = () => {
-    for(let i = 0; i < board.length; i++) {
-        if(i !== 0) {
-            
+    //ROWS
+    for (let i = 0; i < 3; i++) {
+        if(board[i][0] !== 0 && board[i][0] == board[i][1] && board[i][0] == board[i][2]) {
+            return board[i][0]
         }
     }
+
+    //COLUMNS
+    for (let i = 0; i < 3; i++) {
+        if(board[0][i] !== 0 && board[0][i] == board[1][i] && board[0][i] == board[2][i]) {
+            return board[0][i]
+        }
+    }
+
+    //DIAGONAL
+    if(board[0][0] !== 0 && board[0][0] === board[1][1] && board[0][0] == board[2][2]) {
+        return board[0][0]
+    }
+    else if(board[0][2] !== 0 && board[0][2] === board[1][1] && board[0][2] === board[2][0]) {
+        return board[0][2]
+    }
+
+    return -1
 }
 
-//check player turn
+//Displays result screen
+const resultScreen = () => {
+    let results = document.querySelector(".result-screen")
+    results.style.display = "block";
+    
+}
